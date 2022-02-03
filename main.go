fe97a8208d546ad49d3b5ec158d4bb6908b8ae5a
Tamal Saha <tamal@appscode.com>
2022-02-03 15:57:24 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-//	"time"
+	"k8s.io/client-go/rest"
 
-// 	"github.com/nats-io/nats.go"
-// 	"github.com/tamalsaha/nats-hop-demo/shared"
-// 	"github.com/tamalsaha/nats-hop-demo/transport"
+	//	"time"
+
+	// 	"github.com/nats-io/nats.go"
+	// 	"github.com/tamalsaha/nats-hop-demo/shared"
+	// 	"github.com/tamalsaha/nats-hop-demo/transport"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-//	"k8s.io/klog/v2"
+	//	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -21,7 +23,6 @@ import (
 func NewClient() (client.Client, error) {
 	scheme := runtime.NewScheme()
 	_ = clientgoscheme.AddToScheme(scheme)
-	crdinstall.Install(scheme)
 
 	ctrl.SetLogger(klogr.New())
 	cfg := ctrl.GetConfigOrDie()
@@ -31,6 +32,13 @@ func NewClient() (client.Client, error) {
 	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
 	if err != nil {
 		return nil, err
+	}
+
+	cfg.Impersonate = rest.ImpersonationConfig{
+		UserName: "system:serviceaccount:default:default",
+		UID:      "",
+		Groups:   nil,
+		Extra:    nil,
 	}
 
 	return client.New(cfg, client.Options{
@@ -43,11 +51,6 @@ func NewClient() (client.Client, error) {
 	})
 }
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
 func main() {
 	if err := run(); err != nil {
 		panic(err)
@@ -55,39 +58,7 @@ func main() {
 }
 
 func run() error {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-// 	nc, err := nats.Connect(shared.NATS_URL)
-// 	if err != nil {
-// 		klog.Fatalln(err)
-// 	}
-// 	defer nc.Close()
-
-	ctrl.SetLogger(klogr.New())
-	cfg := ctrl.GetConfigOrDie()
-
-// 	tr, err := cfg.TransportConfig()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	cfg.Transport, err = transport.New(tr, nc, "k8s", 10000*time.Second)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
-	if err != nil {
-		return err
-	}
-
-	c, err := client.New(cfg, client.Options{
-		Scheme: scheme,
-		Mapper: mapper,
-		Opts: client.WarningHandlerOptions{
-			SuppressWarnings:   false,
-			AllowDuplicateLogs: false,
-		},
-	})
+	c, err := NewClient()
 	if err != nil {
 		return err
 	}
